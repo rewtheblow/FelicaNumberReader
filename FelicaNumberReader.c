4593f1626e03302ef63bb5ecdbc09da5755d8396
Ryusaburo Tanaka <rew-the-blow@cosmos.ocn.ne.jp>
2012-06-18 02:43:17 +0900

    output the scan results to felicaNumber.log file

--- FelicaNumberReader.c
+++ FelicaNumberReader.c
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////
 //                                                    //
-//   WorkerNumberReader.c                             //
+//   FelicaNumberReader.c                             //
 //                                                    //
 //   This program can read felica's unuque number,    //
 //   cooperates with ferica card reader device.       //
@@ -11,14 +11,33 @@
 ////////////////////////////////////////////////////////
 
 #include <stdio.h>
+#include <time.h>
 #include "felicalib.h"
 
-static void hexdump(uint8 *addr, int n);
+#define OUTPUT_FILE    "felicaNumber.log"
+
+static void hexdump(FILE *fp, uint8 *addr, int n);
 
 int main()
 {
+	FILE *fp;
 	pasori *p;
 	felica *f;
+	char s[256];
+
+	if ((fp = fopen(OUTPUT_FILE, "a")) == NULL) {
+		printf("file open error!!\n");
+		exit(0);
+	}
+	printf("%s\n", "##############################################");
+    printf("##   %s%s   ##\n", "Output file name is ", OUTPUT_FILE);
+	printf("%s\n", "##############################################");
+    
+	fprintf(fp, "%s\n", "##############################################");
+    fprintf(fp, "##   %s%s   ##\n", "Output file name is ", OUTPUT_FILE);
+	fprintf(fp, "%s\n", "##############################################");
+
+    fclose(fp);
 
     p = pasori_open(NULL);
     if (!p) {
@@ -28,20 +47,25 @@ int main()
     pasori_init(p);
 
 	while(1){
+		if ((fp = fopen(OUTPUT_FILE, "a")) == NULL) {
+			printf("file open error!!\n");
+			exit(0);
+		}
+
 	    f = felica_polling(p, POLLING_ANY, 0, 0);
         if (f) {
-            hexdump(f->IDm, 8);
-            printf("\n");
-            hexdump(f->PMm, 8);
-            printf("\n");
-            printf("read succeeded.\n");
+            hexdump(fp, f->IDm, 8);
+            printf("    read succeeded.\n");
+            fprintf(fp, "    read succeeded.\n");
             putchar('\a');
 //      }
 //      else
 //      {
 //          printf("Polling card failed.\n");
         }
+
         sleep(2);
+        fclose(fp);
     }
 
     
@@ -49,10 +73,23 @@ int main()
 	return 0;
 }
 
-static void hexdump(uint8 *addr, int n)
+static void hexdump(FILE *fp, uint8 *addr, int n)
 {
+time_t target_time;
+	struct tm* target_time_tm;
+	char* time_string;
+	int string_length=20;
     int i;
+    	
+	time_string=(char *)malloc(string_length*sizeof(char));
+	(void)time(&target_time);
+	target_time_tm=localtime(&target_time);
+	(void)strftime(time_string,string_length,"%Y/%m/%d %H:%M:%S",target_time_tm);
+
+    printf("%s    ", time_string);
+    fprintf(fp, "%s   ", time_string);
     for (i = 0; i < n; i++) {
         printf("%02X ", addr[i]);
+        fprintf(fp, "%02X ", addr[i]);
     }
 }
